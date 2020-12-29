d3479a289486d06a7b29e2246ff6117c8f012add
yah <wangxiaoerwex@outlook.com>
2020-12-30 02:40:27 +0800

    Psuh(update): 2020-12-30 02:40:20
     M .github/workflows/Build-README.yml
     M init.sh
     M mk-dir.sh

--- init.sh
+++ init.sh
@@ -6,14 +6,15 @@ prin_dir=$(ls -l --time-style=iso |grep "^d" |awk '{print $8}')
 tab=$(echo -e '\t')
 echo "## 文件列表" >> out.md
 echo -e '\n' >> out.md
-echo -e '|目录|名称|链接| \n|:--:|:--:|:--:|' >> out.md
+echo -e '|目录|名称|JSD|GITEE \n|:--:|:--:|:--:|:--:|' >> out.md
 for a in $prin_dir
 do
-  echo  "|$a|$tab|$tab|" >> out.md
+  echo  "|$a|$tab|$tab|$tab|" >> out.md
   for b in $(ls $a)
   do
-    url="https://cdn.jsdelivr.net/gh/wangxiaoerYah/my-jsd-repo@latest/$a/$b"
-    echo "|$tab|$b|$url|" >> out.md
+    jsd="https://cdn.jsdelivr.net/gh/wangxiaoerYah/my-jsd-repo@latest/$a/$b"
+    gitee="https://wangxiaoeryah.gitee.io/my-link-repo/$a/$b"
+    echo "|$tab|$b|$jsde|$gitee|" >> out.md
   done
 done
 
--- mk-dir.sh
+++ mk-dir.sh
@@ -6,10 +6,10 @@ for a in $prin_dir
 do
   cp -rf $a .PUSH-DIR/
 done
-
+rm -rf .GITEE-DIR
+cp -rf .PUSH-DIR .GITEE-DIR 
 if [ -e README.md ]
  then mv -f README.md .PUSH-DIR/
-
 else
   echo "E: 未找到README.md文件"
 fi 
